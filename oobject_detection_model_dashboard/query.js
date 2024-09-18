925d8e620a56b014e2846bd2432796ae715fcb97
smitsharma-420 <smit.insignia@gmail.com>
2024-09-18 19:34:44 +0530

    file changes added

--- oobject_detection_model_dashboard/query.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".ins-view-btn-wrp .filter-btn").forEach((button) => {
-  button.addEventListener("click", function () {
-    document
-      .querySelector(".ins-view-btn-wrp .active")
-      .classList.remove("active");
-    this.classList.add("active");
-  });
-});
